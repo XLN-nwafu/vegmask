0bd9230bf3c58ca74a339cdf7529f3954bc651c3
XLN-nwafu <82072446+XLN-nwafu@users.noreply.github.com>
2024-09-09 18:32:08 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ DEBUG = True
 # 读取数据库环境变量
 username = os.environ.get("MYSQL_USERNAME", 'root')
 password = os.environ.get("MYSQL_PASSWORD", 'root')
-db_address = os.environ.get("MYSQL_ADDRESS", '127.0.0.1:3306')
+db_address = os.environ.get("MYSQL_ADDRESS", '10.27.103.138:3306')
